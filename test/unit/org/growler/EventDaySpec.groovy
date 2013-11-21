39e193ad5bfd96615e32c997e15fe079e41525f6
Rob Bugh <robbugh@gmail.com>
2013-11-20 22:59:51 -0600

    Added EventDay unit test

--- test/unit/org/growler/EventDaySpec.groovy
+++ test/unit/org/growler/EventDaySpec.groovy
@@ -15,6 +15,24 @@ class EventDaySpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
+    void "test valid EventDay"() {
+        when: 'an EventDay is valid'
+        def validateable = new EventDay(date: new Date())
+
+        then: 'validate() returns true and there are no errors'
+        validateable.validate()
+        !validateable.hasErrors()
+        validateable.errors.errorCount == 0
+    }
+    
+    void "test invalid EventDay"() {
+        when: 'an EventDay is missing the date property'
+        def validateable = new EventDay()
+
+        then: 'validate() returns false and there is one error'
+        !validateable.validate()
+        validateable.hasErrors()
+        validateable.errors.errorCount == 1
+        validateable.errors.fieldError.field == 'date'
     }
 }
